0be32334140e633175f5f726d9df0e4532c22c5f
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-08-21 10:19:20 -0700

    socrata-service skeleton

--- lab-taylor/app/service/socrata-service.js
+++ lab-taylor/app/service/socrata-service.js
@@ -8,4 +8,25 @@ function socrataService($log, $q, $http) {
   service.data = [];
 
   const url = `${__API_URL__}`;
+  const config = {
+    headers: {
+      'X-App-Token': `${__SOCRATA_TOKEN__}`
+    }
+
+    service.getData = function() {
+      $log.debug('socrataService.getData')
+
+      return $q((resolve, reject) => {
+        .then(res => {
+          $log.log(`GET ${url}: ${res.status} success`)
+          this.data = res.data
+          resolve(this.data)
+        })
+        .catch(err => {
+          $log.error(`GET ${url}: ${err.status} error`)
+          reject(err)
+        })
+      })
+    }
+  };
 }
